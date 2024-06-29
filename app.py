d49b3de315f773fda5da984e307643d6e6962265
agtm1199 <gulmammadov.tural@gmail.com>
2024-06-29 17:48:56 -0400

    main method

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask('basic app')
 
 @app.route("/")
-def hello_world():
+def main():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=8080
+    )
